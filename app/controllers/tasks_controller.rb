442a5bb0256ad585585f040ee1ed75aee9e45e7c
Kenichi Ukai <rise0646@gmail.com>
2018-09-15 23:30:35 +0900

    tasksのCRUD機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,65 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def new; end
+  def new
+    @task = Task.new
+  end
 
   def show
-    @task = Task.find_by(id: params[:id])
+    @task = get_task
     @plans = @task.plans
   end
 
   def create
+    @task = Task.new(task_params)
+
+    # set current_user
+    @task.user = current_user
+
+    if @task.save
+      flash[:success] = "仕事を追加しました。"
+      redirect_to tasks_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @task = get_task
   end
 
   def update
+    @task = get_task
+
+    @task.update_attributes(task_params)
+
+    if @task.save
+      flash[:success] = "仕事を更新しました。"
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
+
   end
 
-  def destroy; end
+  def destroy
+    @task = get_task
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :end_date)
+  end
+
+  def get_task
+    Task.find_by(id: params[:id])
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.web_console.whitelisted_ips = '172.18.0.1'
+  config.web_console.whitelisted_ips = "172.18.0.1"
+
+  BetterErrors::Middleware.allow_ip! "172.18.0.1"
 end
