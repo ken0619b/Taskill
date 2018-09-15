4d8b7d2c75f6d0f4d335f350bc15040bcdf35250
Kenichi Ukai <rise0646@gmail.com>
2018-09-16 00:57:59 +0900

    Rspec環境を整備

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Relations' do
+    it 'has many plans' do
+      is_expected.to have_many(:plans)
+    end
+  end
 end
